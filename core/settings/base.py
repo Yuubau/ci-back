bde61aa7409432a7d798a1753bd4327deeb3f03f
Dénice <delton.denice@gmail.com>
2023-02-21 22:02:50 +0100

    fix docker-compose

--- core/settings/base.py
+++ core/settings/base.py
@@ -86,17 +86,19 @@ DATABASES = {
         "PASSWORD": 'postgres',
         "HOST": 'database',
         "PORT": 5432,
-    },
+    }
+}
 
-    "workflow": {
-        "ENGINE": "django.db.backends.postgresql",
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+                "ENGINE": "django.db.backends.postgresql",
         "NAME": 'postgres',
         "USER": 'postgres',
         "PASSWORD": 'postgres',
         "HOST": 'localhost',
         "PORT": 5432,
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
